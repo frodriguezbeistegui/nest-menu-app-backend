c89b62801c4286739b56335146772d22c7f00f8c
facundo <frodriguezbeistegui@gmail.com>
2022-08-29 23:04:51 -0300

    mongoose and mongodb added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: 'config.env',
+    }),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a3ryeu7.mongodb.net/?retryWrites=true&w=majority`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
